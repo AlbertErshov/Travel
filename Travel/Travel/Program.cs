27d12bb7266968a95c5e848254bd5fadf3a78109
AlbertErshov <ershov_a@inbox.ru>
2016-06-05 16:48:01 +0400

    first commit

---
+++ Travel/Travel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> tours = new Dictionary<string, string>();
+            tours.Add("Москва", "Париж");
+            tours.Add("Кёльн", "Москва");
+            tours.Add("Мельбурн", "Кёльн");
+            TourList tourList = new TourList(tours);
+            Console.WriteLine("До сортировки: ");
+            foreach (var tour in tourList.Tours)
+            {
+                Console.WriteLine("{0} - {1}", tour.Key, tour.Value);
+            }
+
+            tourList.Sort();
+
+            Console.WriteLine("\nПосле сортировки: ");
+            foreach (var tour in tourList.Tours)
+            {
+                Console.WriteLine("{0} - {1}", tour.Key, tour.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Travel/Travel/TourList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel
+{
+    public class TourList
+    {
+        private Dictionary<string, string> m_tours;
+
+        public TourList(Dictionary<string, string> tours)
+        {
+            m_tours = tours;
+        }
+
+        public Dictionary<string, string> Tours
+        {
+            get { return m_tours; }
+            set { m_tours = value; }
+        }
+        public void Sort()
+        {
+            List<string> starts = m_tours.Keys.Where(x => !m_tours.Values.Contains(x)).ToList();
+
+            if (starts.Count==1)
+            {
+                var start = starts[0];
+                Dictionary<string, string> sortedTours = new Dictionary<string, string>();
+                while (m_tours.Keys.Contains(start))
+                {
+                    var finish = m_tours[start];
+                    sortedTours.Add(start, finish);
+                    start = finish;
+                }
+                m_tours = sortedTours;
+            }
+        }
+    }
+}
